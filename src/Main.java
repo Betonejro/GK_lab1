93b3f06fb885bca9f2f95c2fad6a84c952467119
Betonejro <HotimPL@gmail.com>
2019-02-28 13:08:16 +0100

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+
+import javax.swing.JFrame;
+
+public class Main extends JFrame {
+
+    public static void main(String[] args) {
+        // Marek Mrowiec gr. 2a wariant [1, 4]
+    }
+}
+
